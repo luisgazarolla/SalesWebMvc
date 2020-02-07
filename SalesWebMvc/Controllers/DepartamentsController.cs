56532aeeb20c8791f4973d74fe5ad9fbda4078ad
Luis Gazarolla <luis.gazarolla@gatec.com.br>
2020-02-07 09:30:33 -0300

    First Model-View-Controller - Departament

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament() { ID = 1, NAME = "Eletronics" });
+            list.Add(new Departament() { ID = 2, NAME = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+    }
+}
